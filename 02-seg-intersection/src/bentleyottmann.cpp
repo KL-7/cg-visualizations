d2a792cfad99c63065d3457f7d7fd9f061a46a19
Kirill Lashuk <kirill.lashuk@gmail.com>
2012-03-03 22:19:18 +0300

    Add part of Bentley-Ottmann implementation.

--- 02-seg-intersection/src/bentleyottmann.cpp
+++ 02-seg-intersection/src/bentleyottmann.cpp
@@ -1,23 +1,71 @@
 #include "bentleyottmann.h"
 
-bool pointComp(const Point &a, const Point &b) {
-    return a.point->x() < b.point->x();
+bool pointComp(const Point *a, const Point *b) {
+    return a->p->x() < b->p->x();
 }
 
 QVector<QPointF> BentleyOttmann::intersectionPoints() {
-    PointsSet points(&pointComp);
-    set<QLineF> currentSegments;
+    QPointF *sweepPoint = new QPointF();
+    Point *tmpPoint;
+
+    QVector<Point*> newPoints;
     QVector<QPointF> intersectionPoints;
 
+    PointsSet points(&pointComp);
+    SegmentComparator comp(sweepPoint);
+    SegmentsSet currentSegments(comp);
+    map<QLineF*, int> ranks;
+
     foreach (QLineF segment, m_segments) {
-        points.insert(Point(&segment.p1(), &segment, segment.p1().x() <= segment.p2().x()));
-        points.insert(Point(&segment.p2(), &segment, segment.p2().x() < segment.p1().x()));
+        points.insert(new Point(&segment.p1(), &segment, segment.p1().x() <= segment.p2().x()));
+        points.insert(new Point(&segment.p2(), &segment, segment.p2().x() < segment.p1().x()));
     }
 
+    int currentRank = 0;
+
     while (!points.empty()) {
-        Point p = points.
+        newPoints.clear();
+        Point *p = *points.begin();
+        points.erase(points.begin());
+        sweepPoint->setX(p->p->x());
+        sweepPoint->setY(p->p->y());
+
+        if (p->s2 != 0) {
+
+        } else if (p->left) {
+            int rank = currentRank++;
+            ranks[p->s1] = rank;
+            Segment *s = new Segment(p->s1, rank);
+
+            currentSegments.insert(s);
+
+            SegmentsSet::iterator s1 = currentSegments.upper_bound(s);
+            SegmentsSet::iterator s2 = ++currentSegments.lower_bound(s);
+
+            if (s1 != currentSegments.end() && (tmpPoint = rightIntersection(s, *s1, sweepPoint))) {
+                newPoints << tmpPoint;
+            }
+
+            if (s2 != currentSegments.end() && (tmpPoint = rightIntersection(s, *s2, sweepPoint))) {
+                newPoints << tmpPoint;
+            }
+        } else /* if (!p.left) */ {
+            Segment *s = new Segment(p->s1, ranks[p->s1]);
+
+            SegmentsSet::iterator s1 = currentSegments.upper_bound(s);
+            SegmentsSet::iterator s2 = ++currentSegments.lower_bound(s);
+
+            if (s1 != currentSegments.end() && s2 != currentSegments.end() && (tmpPoint = rightIntersection(*s1, *s2, sweepPoint))) {
+                newPoints << tmpPoint;
+            }
+
+            currentSegments.erase(s);
+        }
     }
 
     return intersectionPoints;
 }
 
+Point* BentleyOttmann::rightIntersection(Segment *s1, Segment *s2, const QPointF *p) {
+    return 0;
+}
--- 02-seg-intersection/src/bentleyottmann.h
+++ 02-seg-intersection/src/bentleyottmann.h
@@ -3,18 +3,43 @@
 
 #include <QtCore>
 #include <set>
+#include <map>
 
 using namespace std;
 
 struct Point {
-    QPointF *point;
-    QLineF *segment;
-    bool isLeftEnd;
+    QPointF *p;
+    QLineF *s1;
+    QLineF *s2;
+    bool left;
 
-    Point(QPointF *point, QLineF *segment, bool isLeftEnd) : point(point), segment(segment), isLeftEnd(isLeftEnd) { }
+    Point(QPointF *point, QLineF *segment1, bool isLeftEnd, QLineF *segment2 = 0)
+        : p(point), s1(segment1), s2(segment2), left(isLeftEnd) { }
 } typedef Point;
 
-typedef set<Point, bool (*) (const Point&, const Point&)> PointsSet;
+struct Segment {
+    QLineF *s;
+    int rank;
+
+    Segment(QLineF *segment, int rank) : s(segment), rank(rank) { }
+
+    qreal high(const QPointF* p) const {
+        return s->p1().y() + (s->p2().y() - s->p1().y()) / (s->p2().x() - s->p1().x()) * (p->x() - s->p1().x());
+    }
+} typedef Segment;
+
+struct SegmentComparator {
+    const QPointF *sweepPoint;
+
+    SegmentComparator(const QPointF *sweepPoint) : sweepPoint(sweepPoint) { }
+
+    bool operator() (const Segment *a, const Segment *b) {
+        return a->high(sweepPoint) == b->high(sweepPoint) ? a->rank < b->rank : a->high(sweepPoint) < b->high(sweepPoint);
+    }
+} typedef SegmentComparator;
+
+typedef set<Point*, bool (*) (const Point*, const Point*)> PointsSet;
+typedef set<Segment*, SegmentComparator> SegmentsSet;
 
 class BentleyOttmann
 {
@@ -24,6 +49,8 @@ public:
 
 private:
     QVector<QLineF> m_segments;
+
+    Point* rightIntersection(Segment *s1, Segment *s2, const QPointF *p);
 };
 
 #endif // BENTLEYOTTMANN_H
